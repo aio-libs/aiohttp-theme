84b2ad32c1a70ae5aef79d09b6f020c90f5a81d5
Andrew Svetlov <andrew.svetlov@gmail.com>
2018-02-07 15:57:24 +0200

    Tune theme

--- aiohttp_theme/support.py
+++ aiohttp_theme/support.py
@@ -4,6 +4,27 @@ from pygments.style import Style
 from pygments.token import Keyword, Name, Comment, String, Error, \
      Number, Operator, Generic, Whitespace, Punctuation, Other, Literal
 
+class DigeratiStyle(Style):
+    background_color = '#444444'
+    styles = {
+        Token:              'noinherit #f0f0f0 bg:#444444',
+        Generic.Subheading: '#ffffff bold',
+        Name.Variable:      'noinherit #cdff00',
+        Generic.Output:     'noinherit #a0a0a0 bg:#303030',
+        Name.Constant:      'noinherit #ff3b77',
+        String:             '#cdff00 italic',
+        Generic.Heading:    '#ffffff bold',
+        Number:             'noinherit #ff3b77',
+        Comment:            '#808080 italic',
+        Name.Function:      '#ffffff bold',
+        Keyword.Type:       'noinherit #77b4c7',
+        Name.Attribute:     '#ffffff bold',
+        Name.Tag:           'noinherit #77b4c7',
+        Name.Entity:        'noinherit #ff3b77',
+        Keyword:            'noinherit #77b4c7',
+        Comment.Preproc:    'noinherit #b8b89f',
+    }
+
 
 # Originally based on FlaskyStyle which was based on 'tango'.
 class Alabaster(Style):
